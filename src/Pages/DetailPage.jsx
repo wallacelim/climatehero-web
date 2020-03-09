0f455f7fe642a06b3160ecc47ff2acca8ae866b2
He <meijie.he@sap.com>
2020-03-09 11:10:02 +0800

    move detailpage to Pages

---
+++ src/Pages/DetailPage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { CustomListItem } from "@ui5/webcomponents-react/lib/CustomListItem";
+import { List, ListMode, ListSeparators, Button, Input, InputType, FlexBox, Text, FlexBoxDirection, FlexBoxAlignItems, FlexBoxJustifyContent, Title, TitleLevel, ButtonDesign, ObjectPage, ObjectPageSection } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
+import { CalendarView } from "../Components/Calendar";
+import { ActivitySummary } from "../Components/ActivitySummary";
+
+export function DetailPage() {
+  const [steps, setSteps] = React.useState(0);
+  const [bikeMins, setBikeMins] = React.useState(0);
+  const [busMins, setBusMins] = React.useState(0);
+  const [veggieMeals, setVeggieMeals] = React.useState(0);
+  const handleStepsChange = React.useCallback((event) => {
+    console.log(event);
+    const step = event.originalEvent.target.value;
+    setSteps(step);
+  }, [setSteps]);
+  const handleBikeMinsChange = React.useCallback((event) => {
+    const bikeMin = event.originalEvent.target.value;
+    setBikeMins(bikeMin);
+  }, [setBikeMins]);
+  const handleBusMinsChange = React.useCallback((event) => {
+    const busMin = event.originalEvent.target.value;
+    setBusMins(busMin);
+  }, [setBusMins]);
+  const handleVeggieMealsChange = React.useCallback((event) => {
+    const veggieMeal = event.originalEvent.target.value;
+    setVeggieMeals(veggieMeal);
+  }, [setVeggieMeals]);
+  const handleClick = e => {
+    e.preventDefault();
+    alert(`Steps ${steps}, Bike Minutes ${bikeMins}, Bus Minutes ${busMins}, VeggieMeasl ${veggieMeals}`);
+  };
+  return (
+    <div>
+      <ObjectPage>
+        <ObjectPageSection title="Calendar View" id="calenderView">
+          <CalendarView />
+        </ObjectPageSection>
+        <ObjectPageSection title="Activity Summary" id="activitySummary">
+          <ActivitySummary style={{width:"100%", ...spacing.sapUiContentPadding}} />
+        </ObjectPageSection>
+      </ObjectPage>
+    </div>
+  );
+}
