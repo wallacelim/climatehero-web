84161e27ac8ebdc38c9f8b92b3d6c98148e70863
Wallace <wallacelim97@gmail.com>
2020-03-16 02:52:25 +0800

    Implement AddGoalModal

---
+++ src/Components/AddGoalModal.jsx
@@ -0,0 +1,202 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+    WALKING,
+    BUS_RIDE,
+    TRAIN_RIDE,
+    BIKE_RIDE,
+    VEGETARIAN_MEAL
+} from "../constants/activityTypes";
+import {
+    Dialog,
+    Select,
+    Input,
+    Option,
+    Button,
+    FlexBox,
+    ButtonDesign,
+    InputType,
+    FlexBoxDirection,
+    FlexBoxJustifyContent,
+    FlexBoxAlignItems,
+    ValueState,
+    CalendarType
+} from "@ui5/webcomponents-react";
+import { DatePicker } from "@ui5/webcomponents-react/lib/DatePicker";
+import {
+    sapUiContentPadding,
+    sapUiTinyMargin,
+    sapUiSmallMarginBottom
+} from "@ui5/webcomponents-react-base/lib/spacing";
+import { addGoal, toggleAddGoalModal } from "../redux/actionCreators";
+import "@ui5/webcomponents-icons/dist/icons/meal.js";
+import "@ui5/webcomponents-icons/dist/icons/passenger-train.js";
+import "@ui5/webcomponents-icons/dist/icons/physical-activity.js";
+import "@ui5/webcomponents-icons/dist/icons/bus-public-transport.js";
+
+const AddGoalModal = ({ showAddGoalModal, toggleAddGoalModal, addGoal }) => {
+    const [name, setName] = useState("");
+    const [target, setTarget] = useState(0);
+    const [activityType, setActivityType] = useState(WALKING);
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleAdd = () => {
+        if (!name) {
+            alert("Please enter a goal name");
+            return;
+        }
+        if (!target) {
+            alert("Please enter a non-zero value");
+            return;
+        }
+        if (!selectedDate) {
+            alert("Please select a valid date");
+            return;
+        }
+        toggleAddGoalModal();
+        const [targetDay, targetMonth, targetYear] = selectedDate.split("/");
+        const targetDate = new Date(targetYear, targetMonth, targetDay);
+        const goal = {
+            name: name,
+            startDate: new Date().toLocaleDateString(),
+            targetDate: targetDate.toLocaleDateString(),
+            type: activityType,
+            currentMeasurement: 0,
+            targetMeasurement: parseInt(target),
+            metric: activityType.metric,
+            progress: 0
+        };
+        addGoal(goal);
+    };
+
+    const handleSelectType = e => {
+        const name = e.parameters.selectedOption.value;
+        switch (name) {
+            case WALKING.name:
+                setActivityType(WALKING);
+                break;
+            case BIKE_RIDE.name:
+                setActivityType(BIKE_RIDE);
+                break;
+            case BUS_RIDE.name:
+                setActivityType(BUS_RIDE);
+                break;
+            case TRAIN_RIDE.name:
+                setActivityType(TRAIN_RIDE);
+                break;
+            case VEGETARIAN_MEAL.name:
+                setActivityType(VEGETARIAN_MEAL);
+                break;
+            default:
+                console.error("Error setting activityType");
+        }
+    };
+
+    return (
+        <Dialog
+            style={sapUiContentPadding}
+            header={[
+                <FlexBox
+                    direction={FlexBoxDirection.Row}
+                    justifyContent={FlexBoxJustifyContent.SpaceBetween}
+                    alignItems={FlexBoxAlignItems.Center}
+                    style={sapUiContentPadding}
+                >
+                    <h5>Add a Goal</h5>
+                    <Button
+                        design={ButtonDesign.Reject}
+                        onClick={toggleAddGoalModal}
+                        style={sapUiTinyMargin}
+                    >
+                        Close
+                    </Button>
+                </FlexBox>
+            ]}
+            stretch={false}
+            open={showAddGoalModal}
+            footer={
+                <div>
+                    <FlexBox
+                        justifyContent={FlexBoxJustifyContent.Center}
+                        style={sapUiTinyMargin}
+                    >
+                        <Button
+                            design={ButtonDesign.Emphasized}
+                            onClick={handleAdd}
+                            style={sapUiTinyMargin}
+                        >
+                            Add
+                        </Button>
+                    </FlexBox>
+                </div>
+            }
+        >
+            <section>
+                <FlexBox
+                    style={{ ...sapUiContentPadding, width: "400px" }}
+                    direction={FlexBoxDirection.Column}
+                    justfyContent={FlexBoxJustifyContent.Center}
+                >
+                    <Input
+                        type={InputType.Text}
+                        onChange={e => setName(e.parameters.value)}
+                        style={sapUiSmallMarginBottom}
+                        placeholder={"Goal Name"}
+                    />
+                    <Select
+                        style={sapUiSmallMarginBottom}
+                        onChange={handleSelectType}
+                    >
+                        <Option icon="physical-activity" value={WALKING.name}>
+                            {WALKING.displayName} ({WALKING.metric})
+                        </Option>
+                        <Option icon="" value={BIKE_RIDE.name}>
+                            {BIKE_RIDE.displayName} ({BIKE_RIDE.metric})
+                        </Option>
+                        <Option
+                            icon="bus-public-transport"
+                            value={BUS_RIDE.name}
+                        >
+                            {BUS_RIDE.displayName} ({BUS_RIDE.metric})
+                        </Option>
+                        <Option icon="passenger-train" value={TRAIN_RIDE.name}>
+                            {TRAIN_RIDE.displayName} ({TRAIN_RIDE.metric})
+                        </Option>
+                        <Option icon="meal" value={VEGETARIAN_MEAL.name}>
+                            {VEGETARIAN_MEAL.displayName} (
+                            {VEGETARIAN_MEAL.metric})
+                        </Option>
+                    </Select>
+                    <Input
+                        type={InputType.Number}
+                        placeholder={"Goal"}
+                        onChange={e => setTarget(e.parameters.value)}
+                        style={sapUiSmallMarginBottom}
+                    />
+                    <DatePicker
+                        valueState={ValueState.None}
+                        formatPattern={"dd/MM/yyyy"}
+                        primaryCalendarType={CalendarType.Gregorian}
+                        disabled={false}
+                        readonly={false}
+                        onChange={date =>
+                            setSelectedDate(date.parameters.value)
+                        }
+                        placeholder={"Target Date of Completion"}
+                    />
+                </FlexBox>
+            </section>
+        </Dialog>
+    );
+};
+
+const mapStateToProps = state => ({
+    showAddGoalModal: state.showAddGoalModal
+});
+
+const mapDispatchToProps = dispatch => ({
+    toggleAddGoalModal: () => dispatch(toggleAddGoalModal()),
+    addGoal: goal => dispatch(addGoal(goal))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddGoalModal);
