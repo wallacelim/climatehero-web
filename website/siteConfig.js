9f16bbc2078f6b74f3853970579e581bd2290e50
I530641 <wallace.lim@sap.com>
2020-05-01 11:45:55 +0800

    Reinitialize Docusaurus

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,36 +8,52 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: 'User1',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/undraw_open_source.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
 const siteConfig = {
-    title: "ClimateHero",
-    tagline: "Be a ClimateHero today!",
-    url: "https://github.wdf.sap.corp/Ie-CoE-SGP/climahtehero-web",
-    baseUrl: "/",
+  title: 'ClimateHero', 
+  tagline: 'Be a ClimateHero today!',
+  url: 'https://github.wdf.sap.corp', 
+  baseUrl: '/climatehero-web/',
 
-    // Used for publishing and more
-    projectName: "climate-hero",
-    organizationName: "SAP IE CoE",
+  // Used for publishing and more
+  projectName: 'climatehero-web',
+  organizationName: 'IE-CoE-SGP',
 
-    headerLinks: [
-        { doc: "doc1", label: "Docs" },
-        { doc: "doc4", label: "API" },
-        { page: "help", label: "Help" },
-        { blog: true, label: "Blog" },
-    ],
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
+  ],
 
-    /* path to images for header/footer */
-    headerIcon: "img/favicon.ico",
-    footerIcon: "img/favicon.ico",
-    favicon: "img/favicon.ico",
+  // If you have users set above, you add it here:
+  users,
 
-    /* Colors for website */
-    colors: {
-        primaryColor: "#66999c",
-        secondaryColor: "#476b6d",
-    },
+  /* path to images for header/footer */
+  headerIcon: 'img/favicon.ico',
+  footerIcon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
 
-    /* Custom fonts for website */
-    /*
+  /* Colors for website */
+  colors: {
+    primaryColor: '#5e519a',
+    secondaryColor: '#41386b',
+  },
+
+  /* Custom fonts for website */
+  /*
   fonts: {
     myFont: [
       "Times New Roman",
@@ -50,25 +66,39 @@ const siteConfig = {
   },
   */
 
-    // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-    copyright: `Copyright © ${new Date().getFullYear()} SAP`,
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} SAP`,
 
-    highlight: {
-        // Highlight.js theme to use for syntax highlighting in code blocks.
-        theme: "default",
-    },
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
 
-    // Add custom scripts here that would be placed in <script> tags.
-    scripts: ["https://buttons.github.io/buttons.js"],
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
 
-    // On page navigation for the current documentation page.
-    onPageNav: "separate",
-    // No .html extensions for paths.
-    cleanUrl: true,
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
 
-    // Open Graph and Twitter card images.
-    ogImage: "img/undraw_online.svg",
-    twitterImage: "img/undraw_tweetstorm.svg",
+  // Open Graph and Twitter card images.
+  ogImage: 'img/undraw_online.svg',
+  twitterImage: 'img/undraw_tweetstorm.svg',
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  // docsSideNavCollapsible: true,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
